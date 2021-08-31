b10d1b97fc31c261f1c65dc6d95a00421f4641cb
Amsal Khan <amsalkhan@iul.ac.in>
2021-08-31 23:23:10 +0000

    kebab: Init Project Xtended Based on Android 11

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kebab.mk
+    $(LOCAL_DIR)/xtended_kebab.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kebab-user \
-    lineage_kebab-userdebug \
-    lineage_kebab-eng
+    xtended_kebab-user \
+    xtended_kebab-userdebug \
+    xtended_kebab-eng
